d4e023fb39fda8d1d40dac4501f4552a275596a4
sebastiann88 <sebastian.nadeau45@gmail.com>
2022-11-24 17:01:59 -0500

    create website

---
+++ greensock.js
@@ -0,0 +1,53 @@
+// Animation
+
+gsap.to('#image-sequence', {
+	scrollTrigger: {
+		trigger: ".animation-fade-out",
+		toggleActions: "play none none reverse"
+	},
+ 	opacity: 0,
+ 	duration: 0.95,
+})
+
+gsap.to('.play-button-container', {
+	scrollTrigger: {
+		trigger: ".animation-fade-out",
+		toggleActions: "play none none reverse"
+	},
+ 	opacity: 0,
+ 	duration: 0.95,
+})
+
+// Portfolio
+
+gsap.to('.portfolio-button-link', {
+	scrollTrigger: {
+		trigger: ".portfolio-fade-in-link",
+		toggleActions: "play none none reverse",
+		toggleClass: {targets: ".portfolio-button-link", className: ".portfolio-hide"}
+
+	},
+ 	duration: 2,
+ 	opacity: 1,
+ 	delay: 1
+})
+
+gsap.to('.portfolio', {
+	scrollTrigger: {
+		trigger: ".portfolio-fade-in",
+		toggleActions: "play none none none",
+	},
+ 	opacity: 1,
+ 	duration: 2,
+ 	delay: 0.10
+})
+
+gsap.to('#container', {
+	scrollTrigger: {
+		trigger: ".portfolio-fade-in",
+		toggleActions: "play none none none",
+	},
+ 	opacity: 1,
+ 	duration: 2,
+ 	delay: 0.10
+})
